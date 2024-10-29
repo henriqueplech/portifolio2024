9891050c15c6b3635529e0d2f55f3d564b70b87f
Henrique Ferrario <henriqueplech1@gmail.com>
2024-10-28 21:26:35 -0300

    Second commit

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,37 +1,29 @@
-import type { Metadata } from "next";
-import localFont from "next/font/local";
+import Header from "@/components/header";
 import "./globals.css";
+import { Inter } from "next/font/google";
 
-const geistSans = localFont({
-  src: "./fonts/GeistVF.woff",
-  variable: "--font-geist-sans",
-  weight: "100 900",
-});
-const geistMono = localFont({
-  src: "./fonts/GeistMonoVF.woff",
-  variable: "--font-geist-mono",
-  weight: "100 900",
-});
 
-export const metadata: Metadata = {
-  title: "Henrique Ferrario | Fullstack Developer",
-  description: "Henrique Ferrario's personal portifolio website",
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata = {
+  title: "Ricardo | Personal Portfolio",
+  description: "Ricardo is a full-stack developer with 8 years of experience.",
 };
 
 export default function RootLayout({
   children,
-}: Readonly<{
+}: {
   children: React.ReactNode;
-}>) {
+}) {
   return (
-    <html lang="en">
+    <html lang="en" className="!scroll-smooth">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} text-gray-500  bg-gray-50 *:antialiased`}
+        className={`${inter.className} bg-gray-50 text-gray-950 relative pt-28 sm:pt-36 dark:bg-gray-900 dark:text-gray-50 dark:text-opacity-90`}
       >
-        <div className="bg-[#fbe2e3] absolute top-[-6rem] right-[11rem] h-[31.25rem] w-[31.25] rounded-full blur-[10rem] sm:w-[68.75rem] "></div>
-        <div className="bg-[#dbd7fb] absolute top-[-1rem] left-[-35rem] h-[31.25rem] w-[31.25] rounded-full blur-[10rem] sm:w-[68.75rem] md:left-[-33rem] lg:left-[-28rem] xl:left-[15rem] 2xl:left-[-5rem] "></div>
+        <div className="bg-[#fbe2e3] absolute top-[-6rem] -z-10 right-[11rem] h-[31.25rem] w-[31.25rem] rounded-full blur-[10rem] sm:w-[68.75rem] dark:bg-[#946263]"></div>
+        <div className="bg-[#dbd7fb] absolute top-[-1rem] -z-10 left-[-35rem] h-[31.25rem] w-[50rem] rounded-full blur-[10rem] sm:w-[68.75rem] md:left-[-33rem] lg:left-[-28rem] xl:left-[-15rem] 2xl:left-[-5rem] dark:bg-[#676394]"></div>
 
-        {children}
+            {children}
       </body>
     </html>
   );
--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,9 @@
+import Intro from "@/components/intro";
 
 export default function Home() {
   return (
-    <div>
-      <h1>Home</h1>
-
-    </div>
+    <main>
+      <Intro />
+    </main>
   );
 }
